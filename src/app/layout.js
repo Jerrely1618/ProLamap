2896a7b700a530baec06b9e03fc139ba45258466
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-10-23 20:24:40 -0400

    Fixed fonts and added favicon logos

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,24 @@
+import Head from 'next/head';
 import './globals.css';
 import { Gamja_Flower, Montserrat, Manrope } from "next/font/google";
 const montserrat = Montserrat({
   subsets: ['latin'],
   display: 'swap',
+  variable: '--font-montserrat',
 });
 
 const gamjaFlower = Gamja_Flower({
   subsets: ['latin'],
   weight: ['400'],
   display: 'swap',
+  variable: '--font-gamja-flower',
 });
 
 const manrope = Manrope({
   subsets: ['latin'],
   weight: ['200', '300', '400', '500', '600', '700', '800'],
   display: 'swap',
+  variable: '--font-manrope',
 });
 
 export const metadata = {
@@ -36,6 +40,14 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${montserrat.className} ${gamjaFlower.className} ${manrope.className}`}>
+      <Head>
+      <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Re" />
+<link rel="manifest" href="/site.webmanifest" />
+</Head>
       <body className={`antialiased`}>
         {children}
       </body>
